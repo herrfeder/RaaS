0716d0c59a6527ea7f7a31494bd1d28d4be1baa7
herrfeder <d.lassig@t-online.de>
2019-07-07 10:52:07 -0700

    Subject: Restructurize DataObject.py   - for beeing able to open and manage multiple DataFrames at once

--- DataObject.py
+++ DataObject.py
@@ -3,17 +3,26 @@ import sqlite3
 import glob
 import time
 import datetime
+import sqlalchemy
 from IPython.core.debugger import Tracer; debug_here = Tracer()
 
 class DataObject():
 
-    def __init__(self,columns,env):
+    def __init__(self,columns,env, datascope):
 
         if columns:
             self.df = pd.DataFrame(columns=columns)
 
         self.dftype = env['dftype']
         self.project = env['project']
+
+		self.conn = sqlalchemy.create_engine("sqlite:///data/"+self.project+".db")
+        self.meta = MetaData(self.conn,reflect=True)
+        self.portscan = self.meta.tables['portscan']
+		self.dirtraversal = self.meta.tables['dirtraversal']
+		self.table_types = ["master","temp"]
+		self.time_stamp = "%Y%m%d%H%M"
+		self.table_name_tpl = "{dftype}_{tabletype}_{timestamp}"
 
     def CreateFromDictList(self,result_list):
         self.df = pd.DataFrame()
@@ -31,24 +40,28 @@ class DataObject():
     def dropIndex(self, index):
         self.df.drop(index, inplace=True)
 
-    def saveToCSV(self):
+	def get_table_name(self, name):
 
-        timedate = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+		pass	
+
+    def save_to_csv(self):
+
+        timedate = datetime.datetime.now().strftime(self.time_stamp)
         self.df.to_csv("data/"+timedate+self.project+"_"+self.dftype+".csv")
 
-    def saveToSqlite(self,append=False):
+    def save_to_sqlite(self, name, append=False):
 
         try:
-            conn = sqlite3.connect("data/"+self.project)
+            conn = sqlite3.connect("data/"+self.project+".db")
             if append:
-                self.df.to_sql(self.dftype, con=conn, if_exists='append')
+                self.df.to_sql(self.get_table_name(name), con=conn, if_exists='append')
             else:
-                self.df.to_sql(self.dftype, con=conn)
+                self.df.to_sql(name, con=conn, if_exists='fail')
 
         except Error as e:
             print(e)
 
-    def loadFromCSV(self):
+    def load_from_csv(self):
 
         files = sorted(glob.glob("data/*"+self.project+"_"+self.dftype+".csv"))
         if len(files) < 1:
@@ -58,11 +71,11 @@ class DataObject():
         self.df = pd.read_csv(load_file)
         self.df.fillna('',inplace=True)
 
-    def loadFromSqlite(self,append=False):
+    def load_from_sqlite(self, name, append=False):
 
         try:
-            conn = sqlite3.connect("data/"+self.project)
-            self.df = pd.read_sql_table(self.dftype, con=conn)
+            conn = sqlite3.connect("data/"+self.project+".db")
+            self.df = pd.read_sql_table(self.get_table_name(name), con=conn)
 
         except Error as e:
             print(e)
@@ -87,7 +100,100 @@ class DataObject():
         for dup in duplicates.iterrows():
             pass
 
+    def return_table(self, table_name):
+        
+        if table_name not in self.conn.table_names():
+            return "Wrong tablename"
+        else:
+            return pd.read_sql_table(table_name, self.conn)
+        
+    def return_domain_list(self):
+        
+        return pd.read_sql_table("subdomain", self.conn)['domain'].unique()
+    
+    def return_ip_list(self):
+        
+        return pd.read_sql_table("subdomain", self.conn)['ip4_1'].unique()
+    
+    def return_dirtraversal(self, domain):
+        '''
+        Returns a directory traversal scan for one or multiple Domains. As a single IP could be used for multiple domains.
+        '''
+        final_result = {}
+        stats_result = {}
+        overview_dt = "### <span style='color:#606200'>Dirtraversal Stats\n</span>"
 
+        for single_domain in domain:
+            s = select([self.dirtraversal]).where(self.dirtraversal.c.domain == single_domain)
+            temp_result = pd.read_sql(s, self.conn)
+            if temp_result.shape[0] > 0:
+                stats_result[single_domain+'_number'] = temp_result.shape[0]
+                stats_result[single_domain+'_200'] = temp_result[temp_result.status == "200"].shape[0]
+                stats_result[single_domain+'_401'] = temp_result[temp_result.status == "401"].shape[0]
+                stats_result[single_domain+'_403'] = temp_result[temp_result.status == "403"].shape[0]
+
+                final_result[single_domain] = temp_result
+                test=single_domain+'_'+'403'
+                overview_dt = overview_dt + overview_dt_t.format(single_domain,
+                                                                   stats_result[single_domain+'_number'],
+                                                                   stats_result[single_domain+'_200'],
+                                                                   stats_result[single_domain+'_401'],
+                                                                   stats_result[single_domain+'_403'])
+        return (final_result, overview_dt)
+
+    
+    def return_portscan(self, ip):
+
+        stats_result = {}
+
+        s = select([self.portscan]).where(self.portscan.c.ip == ip)
+        result = pd.read_sql(s, self.conn)
+        if result.shape[0] > 1:
+            df = extract_scan(result.iloc[0])
+            
+			stats_result['open'] = df[df.state == "open"].shape[0]
+            stats_result['filtered'] = df[df.state == "filtered"].shape[0]
+            
+            overview_ps = overview_ps_t.format(ip,
+                                               stats_result['open'],
+                                               stats_result['filtered'])
+            
+            return (df, overview_ps)
+        
+        elif result.shape[0] == 0:
+            return pd.DataFrame()
+        
+        else:
+            df = extract_scan(result)
+            
+            stats_result['open'] = df[df.status == "open"].shape[0]
+            stats_result['filtered'] = df[df.status == "filtered"].shape[0]
+            
+            overview_ps = overview_ps_t.format(ip,
+                                               stats_result['open'],
+                                               stats_result['filtered'])
+            
+            
+            return (df, overview_ps)
+            
+        
+    def ip_to_domain(self, ip):
+        
+        df = self.return_table("subdomain")
+        
+        if not df.empty:
+            return list(df.query("ip4_1==@ip")["domain"].values)
+        else:
+            return ""
+
+    def domain_to_ip(self, domain):
+        
+        df = self.return_table("subdomain")
+        
+        if not df.empty:
+            return df.query("domain==@domain")["ip4_1"].values
+        else:
+return ""
 
 if __name__ == "__main__":
     do = DataObject(["blah"],"blah","blah")
--- datasupport.py
+++ datasupport.py
@@ -4,26 +4,38 @@ from IPython.core.debugger import Tracer; debughere = Tracer()
 import json
 
 
-def extract_scan(df, ip="", protocol="tcp"):
-    if ip:
+def extract_scan(df ,ip="",protocol='tcp'):
+    no_scan = []
+    if ip: 
         scan = df[df["ip"] == ip][protocol].iloc[0]
     else:
         if not isinstance(df,pd.core.series.Series):
             raise WrongDataFrameSize
-            return -1
+            pass
         else:
             scan = df[protocol]
-    if scan:
+    
+    scan_df = pd.DataFrame(columns=['conf', 'cpe', 'extrainfo', 'name', 'port', 'product', 'reason', 'state', 'version'])
+    
+    if isinstance(scan,str):
         scan = json.loads(scan)
-    else:
-        raise NoScanAvailable
-        return -1
+        scan = [dict(scan[x],**{"port":x}) for x in list(scan.keys()) ]
+        scan_df = scan_df.append(scan[0], ignore_index=True)
+        
+        
+        if len(scan) > 1:
+            scan_df = scan_df.append(scan[1:], ignore_index=True)
+            scan_df.insert(0,"ip",ip, allow_duplicates=True)
+            return scan_df
+        else:
+            scan_df.insert(0,"ip",ip, allow_duplicates=True)
+            return scan_df
 
-    scan = [dict(scan[x],**{"port":x}) for x in list(scan.keys()) ]
-    scan_df = pd.DataFrame([scan[0]])
-    if len(scan) > 1:
-        return scan_df.append(scan[1:], ignore_index=True)
+    
     else:
+        
+        scan_df = scan_df.append({col: "noscan" for col in scan_df.columns}, ignore_index=True)
+        scan_df.insert(0,"ip",ip, allow_duplicates=True)
         return scan_df
 
 
